c5f14fec2226110791b7ed5412507792658ed471
promethee <lunette.francois@gmail.com>
2021-06-18 15:08:09 +0200

    Update install.rpi.sh

--- install.rpi.sh
+++ install.rpi.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo apt-get update
 sudo apt-get install -y libatlas-base-dev libhdf5-dev libgtk2.0-dev libgtk-3-dev libopenjp2-7-dev libilmbase-dev libilmbase23 libopenexr-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev python-rpi.gpio python-spidev python-pip
-pip3 install st7789 Pillow Flask
+pip3 install st7789 spidev Pillow Flask
